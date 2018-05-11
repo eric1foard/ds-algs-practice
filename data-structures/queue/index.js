f5a8355fa5468a23020705a4cf0525063b3d7887
Eric Foard <foard265@gmail.com>
2018-05-11 09:28:16 -0700

    progress

--- data-structures/queue/index.js
+++ data-structures/queue/index.js
@@ -1,5 +1,43 @@
-class Queue {
-
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
-module.exports = Queue;
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    enqueue(value) {
+        let node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.next = this.tail = node;
+        }
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        let { value } = this.head;
+        if (this.head === this.tail) {
+            this.tail = null;
+        }
+        this.head = this.head.next;
+        return value;
+    }
+
+    isEmpty() {
+        return this.head === null;
+    }
+}
+
+module.exports = {
+    Queue,
+    Node
+};
--- data-structures/queue/index.test.js
+++ data-structures/queue/index.test.js
@@ -1,9 +1,86 @@
-const Queue = require('./index');
+const { Queue, Node } = require('./index');
+
+let q;
+
+beforeEach(() => {
+  q = new Queue();
+});
 
 test('Queue should be defined', () => {
   expect(Queue).toBeDefined();
 });
 
 test('queue should have head and tail props when invoked as a constructor', () => {
-  let q = new Queue();
+  expect(q.head).toBeDefined();
+  expect(q.tail).toBeDefined();
+});
+
+test('queue should have an isEmpty method', () => {
+  expect(typeof q.isEmpty).toBe('function'); 
+});
+
+test('isEmpty should return true when queue is empty', () => {
+  expect(q.isEmpty()).toBeTruthy();
+});
+
+test('isEmpty should return false when queue is non-empty', () => {
+  q.enqueue(2);
+  expect(q.isEmpty()).toBeFalsy();
+});
+
+test('queue should have an enqueue method', () => {
+  expect(typeof q.enqueue).toBe('function');
+});
+
+test('enqueue should correctly insert element into an empty queue', () => {
+  q.enqueue(3);
+  expect(q.head).toEqual(q.tail);
+  expect(q.head instanceof Node).toBeTruthy();
+  expect(q.head.value).toEqual(3);
+  expect(q.head.next).toBeNull();
+});
+
+test('queue should correctly insert element into a non-empty queue', () => {
+  q.enqueue(3);
+  expect(q.head).toEqual(q.tail);
+  q.enqueue(7);
+  expect(q.head).not.toEqual(q.tail);
+  expect(q.head.value).toEqual(3);
+  expect(q.tail.value).toEqual(7);
+  expect(q.head.next.value).toEqual(7);
+  expect(q.tail.next).toBeNull();
+});
+
+test('queue should have a dequeue method', () => {
+  expect(typeof q.dequeue).toEqual('function');
+});
+
+test('dequeue should return null for an empty queue', () => {
+  expect(q.dequeue()).toBeNull();
+});
+
+test('dequeue should not modify an empty queue', () => {
+  expect(q.head).toBeNull();
+  expect(q.tail).toBeNull();
+});
+
+test('dequeue should return correct value when queue has single element', () => {
+  q.enqueue(1);
+  expect(q.dequeue()).toEqual(1);
+});
+
+test('dequeue should correctly modify queue structure when queue has one element', () => {
+  q.enqueue(1);
+  q.dequeue();
+  expect(q.head).toBeNull();
+  expect(q.tail).toBeNull();
+});
+
+test('dequeue should return correct value when queue has multiple elements', () => {
+  [0,1,2,3].forEach(v => q.enqueue(v));
+  expect(q.dequeue()).toEqual(0);
+});
+
+test('dequue should correctly modify queue structure when queue has multiple elements', () => {
+
 });
