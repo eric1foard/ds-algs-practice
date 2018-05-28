6bd5cebe27d46f629658bff0585c9b2df811e1fb
Eric Foard <foard265@gmail.com>
2018-05-27 17:19:08 -0700

    sort test refactor

--- algorithms/sorting/merge.test.js
+++ algorithms/sorting/merge.test.js
@@ -1,19 +1,4 @@
 const { sort } = require('./merge');
+const sortTests = require('./test-common');
 
-describe('insertion sort', () => {
-    it('should return an empty array when array is empty', () => {
-        expect(sort([])).toEqual([]);
-    });
-    it('should correctly sort array of size 1', () => {
-        expect(sort([1])).toEqual([1]);
-    });
-    it('should correctly sort an already-sorted array', () => {
-        expect(sort([1,2,3,4,5])).toEqual([1,2,3,4,5]);
-    });
-    it('should sort an array in reverse-sorted order (worst case)', () => {
-        expect(sort([5,4,3,2,1])).toEqual([1,2,3,4,5]); 
-    });
-    it('should correctly sort an array in random order', () => {
-        expect(sort([4,1,2,5,3])).toEqual([1,2,3,4,5]);
-    });
-});
+sortTests(sort, 'merge');
---
+++ algorithms/sorting/test-common.js
@@ -0,0 +1,19 @@
+module.exports = (sort, sortAlgName) => {
+    describe(sortAlgName, () => {
+        it('should return an empty array when array is empty', () => {
+            expect(sort([])).toEqual([]);
+        });
+        it('should correctly sort array of size 1', () => {
+            expect(sort([1])).toEqual([1]);
+        });
+        it('should correctly sort an already-sorted array', () => {
+            expect(sort([1,2,3,4,5])).toEqual([1,2,3,4,5]);
+        });
+        it('should sort an array in reverse-sorted order', () => {
+            expect(sort([5,4,3,2,1])).toEqual([1,2,3,4,5]); 
+        });
+        it('should correctly sort an array in random order', () => {
+            expect(sort([4,1,2,5,3])).toEqual([1,2,3,4,5]);
+        });
+    });
+}
